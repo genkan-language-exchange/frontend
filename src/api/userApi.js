c7160f19a45451453a5b9df0f6e91a805cb32570
jesse <fkng3rmz@gmail.com>
2021-03-26 05:45:17 -0400

    api prefixes

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// const prefix = 'https://genkan.herokuapp.com/api/v1/users'
-const prefix = 'http://localhost:5000/api/v1/users'
+const prefix = 'https://genkan.herokuapp.com/api/v1/users'
+// const prefix = 'http://localhost:5000/api/v1/users'
 const token = localStorage.getItem('genkan-token')
 const config = {
   headers: {
